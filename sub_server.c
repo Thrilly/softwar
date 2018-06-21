2f1d8a9270cbca1a7a7439e7f347d05ce22c55cc
Nicolas <nico.joachim@free.fr>
2018-06-21 22:34:11 +0200

    init 2

--- sub_server.c
+++ sub_server.c
@@ -2,16 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc < 3) {
-    printf("Port number and channel are mandatory\n");
+  if (argc < 4) {
+    printf("Port number, ip and channel are mandatory\n");
     return 0;
   }
 
   zsock_t *client = zsock_new(ZMQ_SUB);
-  zsock_connect(client, "tcp://127.0.0.1:%s", argv[1]);
-  printf("Client connected to 127.0.0.1:%s\n", argv[1]);
+  zsock_connect(client, "tcp://%s:%s", argv[1], argv[2]);
+  printf("Client connected to %s:%s\n", argv[1], argv[2]);
 
-  zsock_set_subscribe(client, argv[2]);
+  zsock_set_subscribe(client, argv[3]);
   zsock_set_subscribe(client, "#general:");
 
   while (!zsys_interrupted) {
